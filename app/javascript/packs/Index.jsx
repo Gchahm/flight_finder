4ca783f712f5783a3414b8e0393c4c7470fa7dc6
GFrancelino <gustavo.francelino@gmail.com>
2021-01-02 19:28:22 +0000

    move react to its own folder

--- app/javascript/packs/Index.jsx
+++ app/javascript/packs/Index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { render } from "react-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
-import App from "../components/App";
+import App from "../react/components/App";
 
 document.addEventListener("DOMContentLoaded", () => {
   render(
