1109b28beb29f18d373473ee43f0bd999e2cd553
Gustavo Francelino <gustavo.francelino@gmail.com>
2021-01-06 18:34:45 +0000

    add api for flight search

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      get 'flights/search'
+    end
+  end
   root 'spa#index'
   get '/*path' => 'spa#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
